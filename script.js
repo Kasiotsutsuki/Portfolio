0e7429f7bd3a471ea5384dae8a22d2387bc8b198
Kasiotsutsuki <139860870+Kasiotsutsuki@users.noreply.github.com>
2023-08-31 20:50:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-input", {
+       strings: [ "Kasi Vishwanath", "Web Developer", "UI Designer" , "Android Developer" ],
+       typeSpeed :100,
+       backSpeed :100,
+       loop: true,
+})
